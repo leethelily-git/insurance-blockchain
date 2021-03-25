4b4fa7adbf4087cb74ea2c7c68e93775317fdc51
Bing Huang <bing2609@gmail.com>
2021-03-25 17:46:13 +0800

    app: included /admin route to serve admin view

--- insurance-application/policyholder_app/src/App.js
+++ insurance-application/policyholder_app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import User from './views/User'
+import Admin from './views/Admin'
 
 export default () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={User} />
+      <Route path="/admin" component={Admin} />
     </Switch>
   </BrowserRouter>
 )
